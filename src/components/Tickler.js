e2e70dc703d86cf6881c98d0d0f37f5228850fc0
barakatimothy <timothybaraka39@gmail.com>
2023-07-20 16:52:13 -0400

    complete!

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,35 @@
 import React from "react";
 
-function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+function Tickler(){
+  
+  function tickle(event ){
+    console.log(event)
   }
-
-  return <button>Tickle me!</button>;
+  return <button onClick= {tickle}>Tickle me!</button>
 }
 
 export default Tickler;
+
+
+
+// function Clickable(){
+//   return <button>Click Me !</button>
+// }
+
+// function App(){
+//   function handleClick(){
+//     console.log("click")
+//   }
+//   return <Clickable onClick ={handleClick}/>
+// }
+
+
+// function Clickable(){
+//   function handleClick(){
+//     console.log("click");
+//   }
+//  return <button onClick={handleClick}>Click Me !</button>
+// }
+// function App(){
+//   return <Clickable/>
+// }
